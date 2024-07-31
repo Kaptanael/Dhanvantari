32faac82f9af49ea587f73d020a160e7f48b56c0
Md. Atikur Zaman <arzaman@ael-bd.com>
2024-07-31 16:03:52 +0600

    add api project template

---
+++ Services/Patient/Patient.Api/Controllers/V1/HomeController.cs
@@ -0,0 +1,19 @@
+﻿namespace Patient.Api.Controllers.V1;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "Home")]
+    public IActionResult Home()
+    {
+        return Ok("Home");
+    }
+}
